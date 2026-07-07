c3da5b2526df37ce8ad74867e58d7e6f17bab2e9
captivator <84224501+qaptivator@users.noreply.github.com>
2026-07-07 15:24:51 +0300

    added pinia

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,7 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineNuxtConfig({
 	compatibilityDate: '2025-07-15',
 	devtools: { enabled: true },
-	modules: ['nuxt-lucide-icons'],
+	modules: ['nuxt-lucide-icons', '@pinia/nuxt'],
 	css: ['~/assets/css/main.css'],
 	lucide: {
 		namePrefix: 'Icon',
