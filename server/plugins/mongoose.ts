f263cb229b2a42e0e7648f9be974e745aeb124a8
captivator <84224501+qaptivator@users.noreply.github.com>
2026-08-03 19:08:32 +0300

    fixed mongodb connection issue

--- server/plugins/mongoose.ts
+++ server/plugins/mongoose.ts
@@ -1,6 +1,12 @@
 import mongoose from 'mongoose'
+import { setServers } from 'node:dns'
 
 export default defineNitroPlugin(async () => {
+	// forces Node's internal resolver to bypass the broken system stack
+	// (fixes MongoDB Connection Error: querySrv ECONNREFUSED _mongodb._tcp.cluster0)
+	// ((that is a known dns error in NodeJS v24.18.0 LTS))
+	setServers(['1.1.1.1', '8.8.8.8'])
+
 	const config = useRuntimeConfig()
 	if (!config.mongodbUri) {
 		console.error('MongoDB Connection Error: MongoDB URI not provided')
