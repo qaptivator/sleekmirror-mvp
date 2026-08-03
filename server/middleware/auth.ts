4be050f7e40db5aafc1317c5d2a4ebef0b899013
captivator <84224501+qaptivator@users.noreply.github.com>
2026-08-03 19:56:00 +0300

    crucial id string to objectid fix

--- server/middleware/auth.ts
+++ server/middleware/auth.ts
@@ -1,3 +1,5 @@
+import mongoose from 'mongoose'
+
 export default defineEventHandler((event) => {
 	if (event.path.startsWith('/api/')) {
 		//const authHeader = getRequestHeader(event, 'authorization')
@@ -5,7 +7,7 @@ export default defineEventHandler((event) => {
 		//event.context.user = authHeader ? { authenticated: true } : null
 		// simulate user auth and resolving
 		event.context.user = {
-			_id: '6a4bb0da27b09ebd780971a8',
+			_id: new mongoose.Types.ObjectId('6a4bb0da27b09ebd780971a8'),
 			identifiers: ['email:user@example.com'],
 			creidts: 10,
 			firstName: 'John',
