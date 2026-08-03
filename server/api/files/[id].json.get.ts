6cd36bfaca593bf148a034cb5bcfc9a0170c99f2
captivator <84224501+qaptivator@users.noreply.github.com>
2026-08-03 20:56:47 +0300

    ah okay i see file id isnt parsed correctly

--- server/api/files/[id].json.get.ts
+++ server/api/files/[id].json.get.ts
@@ -7,18 +7,12 @@ export default defineEventHandler(async (event) => {
 
 	const fileId = getRouterParam(event, 'id')
 
-	console.log('Looking for file:', fileId)
-	console.log('For user:', currentUser._id)
-	console.log('User _id type:', typeof currentUser._id)
-
 	const fileInfo = await (File.findOne as any)({
 		_id: fileId,
 		user: currentUser._id,
 	})
 		.select('-binaryData') // Keep it lightweight
 		.lean()
-
-	console.log('Found:', fileInfo) // ADD THIS
 
 	if (!fileInfo)
 		throw createError({ statusCode: 404, statusMessage: 'File not found' })
